8dbe2d12755cb5acdd009b277a6f7e135f38cf5f
MirageM <imiragesz@gmail.com>
2023-09-16 23:21:49 -0400

    {l:1, i:1, s:1,t:1,e:1,n:1}

--- isAnagram.py
+++ isAnagram.py
@@ -8,3 +8,21 @@ def isAnagram(word1, word2):
     return dict1 == dict2
 
 print(isAnagram("listen", "silent"))
+
+
+"""
+l: 1
+i: 1
+s: 1
+t: 1
+e: 1
+n: 1
+
+s: 1
+i: 1
+l: 1
+e: 1
+n: 1
+t: 1
+
+"""
