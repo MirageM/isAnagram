d3f49826fa9d6010c7482194f77a6aeb4059e696
MirageM <imiragesz@gmail.com>
2023-09-21 22:42:50 -0400

    dict1[char] = dict1.get(char, 0) + 1 dic2[char] = dict2.get(char, 0) + 1

---
+++ anagramredo.py
@@ -0,0 +1,9 @@
+def isAnagram(word1, word2):
+	dict1 = {}
+	dict2 = {}
+	for char in word1:
+		dict1[char] = dict1.get(char, 0) + 1
+	for char in word2:
+		dict2[char] = dict2.get(char, 0) + 1
+
+	return dict1 == dict2
